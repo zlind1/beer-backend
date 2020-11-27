7e7f6d85bb1b370f22008a8f3fcc5aeb56df30cb
zlind1 <zlind@ucsc.edu>
2020-11-26 22:25:47 -0800

    added heroku url to openapi.yaml, serving at process.env.PORT in server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require('./app.js');
 
-app.listen(3010, () => {
+app.listen(process.env.PORT || 3010, () => {
   console.log('CSE183 Assignment 7 Server Running');
-  console.log('API Testing UI is at: http://localhost:3010/v0/api-docs/');
+  const url = process.env.PORT ? 'https://zlind-beers.herokuapp.com/v0/api-docs'
+    : 'http://localhost:3010/v0/api-docs/';
+  console.log(`API Testing UI is at: ${url}`);
 });
